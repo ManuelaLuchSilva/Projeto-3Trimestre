166c46bdab9e9eba8d8f2133cab36b2240564d10
ManuelaLuchSilva <manuelaluchtenbergsilva.ifc@gmail.com>
2023-11-30 19:41:47 -0300

    correção js

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,77 +1,84 @@
 let result = document.getElementById("resultado");
+let resultado = 0;
 let operacao = "";
 let stop = false;
-let history = document.getElementById("history")
+let history = document.getElementById("history");
 
 function limpar() {
-    result.value = "";
-    history.innerHTML = "";
-    operacao = "";
+  result.value = "";
+  history.innerHTML = "";
+  operacao = "";
 }
 
 function add_numero(string) {
-    if (!stop) {
-        if (string === "." && result.value.indexOf(".") != -1) {
-            result.value == result.value + string;
-            history.value == result.value;
-        }
-    } else {
-        if (string === "." && result.value.indexOf(".") === -1) {
-            result.value = "";
-            stop = false;
-            history.innerHTML = "";
-            operacao = "";
-            result.value = result.value + string;
-        }
+  if (!stop) {
+    if (string === "." && result.value.indexOf(".") === -1) {
+      result.value = result.value + string;
+      history.innerHTML = result.value;
+    } else if (string !== ".") {
+      result.value = result.value + string;
+      history.innerHTML = result.value;
     }
+  } else {
+    if (string === "." && result.value.indexOf(".") === -1) {
+      result.value = "";
+      stop = false;
+      history.innerHTML = "";
+      operacao = "";
+      result.value = result.value + string;
+    }
+  }
 }
 
 function add_operador(op) {
-    if (result.value == "") {
-        result.value = "0"
-    }
-    if (operacao == "") {
-        operacao = op;
-        result.value = result.value + op;
-    } else {
-        total()
-        result.value = result.value + op;
-        operacao = op;
-        stop = false;
-    }
+  if (operacao === "") {
+    operacao = op;
+    result.value = result.value + op;
+  } else {
+    total();
+    result.value = result.value + op;
+    operacao = op;
+    stop = false;
+  }
 }
 
 function total() {
-    if (!stop) {
-        let numeros = result.value.split(operacao);
+  if (!stop) {
+    let numeros = result.value.split(operacao);
 
-        if (numeros[1] == "") {
-            result.value = result.value + "0";
-            numeros[1] = 0;
-
-        }
-
-        document.getElementById("history").innerHTML = result.value;
-
-        switch (operacao) {
-            case "+":
-                result.value = Number(numeros[0]) + Number(numeros[1]);
-                break;
-
-            case "-":
-                result.value = numeros[0] - numeros[1];
-                break;
-
-            case "*":
-                result.value = numeros[0] * numeros[1];
-                break;
-
-            case "/":
-                result.value = numeros[0] / numeros[1];
-                break;
-        }
-
-
-        stop = true;
+    if (numeros[1] == "") {
+      result.value = result.value + "0";
+      numeros[1] = 0;
     }
+
+    document.getElementById("history").innerHTML = result.value;
+
+    switch (operacao) {
+      case "+":
+        resultado = Number(numeros[0]) + Number(numeros[1]);
+        result.value = Number(numeros[0]) + Number(numeros[1])
+        document.getElementById("history").innerHTML = Number(numeros[0]) + " + " + Number(numeros[1]) + " = " + resultado
+        break;
+
+      case "-":
+        resultado = numeros[0] - numeros[1];
+        result.value = Number(numeros[0]) - Number(numeros[1])
+        document.getElementById("history").innerHTML = Number(numeros[0]) + " - " + Number(numeros[1]) + " = " + resultado
+        break;
+
+      case "*":
+        resultado = numeros[0] * numeros[1];
+        result.value = Number(numeros[0]) * Number(numeros[1])
+        document.getElementById("history").innerHTML = Number(numeros[0]) + " x " + Number(numeros[1]) + " = " + resultado
+        break;
+
+      case "/":
+        resultado = numeros[0] / numeros[1];
+        result.value = Number(numeros[0]) / Number(numeros[1])
+        document.getElementById("history").innerHTML = Number(numeros[0]) + " / " + Number(numeros[1]) + " = " + resultado
+        break;
+    }
+
+    stop = true;
+  }
 }
