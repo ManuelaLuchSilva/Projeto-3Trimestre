c03fd442708a0e8123a44527193de9970d208e61
ManuelaLuchSilva <manuelaluchtenbergsilva@gmail.com>
2023-12-06 08:49:44 -0300

    try fix

--- calculadora/calEspecial.js
+++ calculadora/calEspecial.js
@@ -1,29 +1,29 @@
 function opEspecial() {
-  alert("Aqui esta mais opções de calculos:");
-  let num1 = Number(prompt("Qual o primeiro digito?").replace(",", "."));
-  let op = prompt(
-    "Qual a operação desejada? \n( !; ^; ²√'raiz')"
-  ).toLowerCase();
-
-  let result = 1;
-  if (op == "!") {
-    if (num1 === 0 || num1 === 1) {
-      alert(num1 + "!" + " = 1");
-    }
-    for (let contador = num1; contador >= 1; contador--) {
-      result = result * contador;
-    }
-    alert(num1 + "!" + " = " + result);
-  } else if (op == "raiz") {
-    let result = Math.sqrt(num1);
-    alert("²√" + num1 + " = " + result);
-  } else {
-    let num2 = Number(prompt("Qual o segundo digito?").replace(",", "."));
-    if (op == "^") {
-      result = num1 ** num2;
-      alert(num1 + "^" + num2 + "= " + result);
+    alert("Aqui esta mais opções de calculos:");
+    let num1 = Number(prompt("Qual o primeiro digito?").replace(",", "."));
+    let op = prompt(
+      "Qual a operação desejada? \n( !; ^; ²√'raiz')"
+    ).toLowerCase();
+  
+    let result = 1;
+    if (op == "!") {
+      if (num1 === 0 || num1 === 1) {
+        alert(num1 + "!" + " = 1");
+      }
+      for (let contador = num1; contador >= 1; contador--) {
+        result = result * contador;
+      }
+      alert(num1 + "!" + " = " + result);
+    } else if (op == "raiz") {
+      let result = Math.sqrt(num1);
+      alert("²√" + num1 + " = " + result);
     } else {
-      alert("Resposta invalida!");
+      let num2 = Number(prompt("Qual o segundo digito?").replace(",", "."));
+      if (op == "^") {
+        result = num1 ** num2;
+        alert(num1 + "^" + num2 + "= " + result);
+      } else {
+        alert("Resposta invalida!");
+      }
     }
-  }
-}
+  }  
